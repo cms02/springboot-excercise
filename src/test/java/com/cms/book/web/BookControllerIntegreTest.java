7850b06ee936cb70684ab90395864c5d7ef57abd
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-18 13:15:02 +0900

    단위테스트 -> 통합테스트

--- src/test/java/com/cms/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/cms/book/web/BookControllerIntegreTest.java
@@ -1,11 +1,25 @@
 package com.cms.book.web;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.cms.book.domain.Book;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 
@@ -19,6 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
  *
  */
 
+@Slf4j
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK) // 실제 톰캣을 올리는게 아니라, 다른 톰캣으로 테스트
@@ -26,5 +41,28 @@ public class BookControllerIntegreTest {
 	
 	@Autowired
 	private MockMvc mockMvc;
-
+	
+	//BDDMockito 패턴 given, when, then
+	@Test
+	public void save_테스트() throws Exception{
+		// given (테스트를 하기 위한 준비)
+		Book book = new Book(null, "스프링 따라하기","cms");
+		String content = new ObjectMapper().writeValueAsString(book);
+//		when(bookService.저장하기(book)).thenReturn(new Book(1L,"스프링 따라하기","cms")); 필요없음 실제 service가 수행
+		
+		//when (테스트 실행)
+		ResultActions resultAction = mockMvc.perform(post("/book")
+						.contentType(MediaType.APPLICATION_JSON_UTF8)
+						.content(content)
+						.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then (검증)
+		resultAction
+			.andExpect(status().isCreated()) //201
+			.andExpect(jsonPath("$.title").value("스프링 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+			
+			
+		
+	}
 }
