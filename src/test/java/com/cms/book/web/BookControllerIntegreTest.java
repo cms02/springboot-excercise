7caf67b19bcb4b8a9a7a038e2c98f2d20d82913d
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-18 10:42:43 +0900

    Junit 테스트 설정

---
+++ src/test/java/com/cms/book/web/BookControllerIntegreTest.java
@@ -0,0 +1,30 @@
+package com.cms.book.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+/**
+ * //통합 테스트(모든 Bean들을 똑같이 IoC 올리고 테스트 하는 것)
+ * WebEnvironment.MOCK = 실제 톰캣을 올리는게 아니라, 다른 톰캣으로 테스트
+ * WebEnvironment.RANDOM_POR = 실제 톰캣으로 테스트
+ * @AutoConfigureMockMvc MockMvc를 IoC에 등록해줌.
+ * @Transactional 각각의 테스트함수가 종료될 때마다 트랜잭션을 rollback해주는 어노테이션!
+ * 
+ *
+ */
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK) // 실제 톰캣을 올리는게 아니라, 다른 톰캣으로 테스트
+public class BookControllerIntegreTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+}
---
+++ src/test/java/com/cms/book/web/BookControllerUnitTest.java
@@ -0,0 +1,13 @@
+package com.cms.book.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+//단위 테스트( Controller 관련 로직만 띄우기, Filter , ControllerAdviceFilter ...) 
+
+@WebMvcTest
+public class BookControllerUnitTest {
+
+
+}
