5f8d46f84e9aacebfe793819a668b6bf364ef275
DESKTOP-DNDTM1N\r2ware <minoui1234@gmail.com>
2021-11-18 14:29:01 +0900

    unit / integration test

--- src/test/java/com/cms/book/web/BookControllerIntegreTest.java
+++ src/test/java/com/cms/book/web/BookControllerIntegreTest.java
@@ -1,10 +1,21 @@
 package com.cms.book.web;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,11 +23,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cms.book.domain.Book;
+import com.cms.book.domain.BookRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
@@ -42,6 +55,18 @@ public class BookControllerIntegreTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	@BeforeEach //메소드 실행 전 increment 초기화
+	public void init() {
+//		entityManager.createNativeQuery("ALTER TABLE book AUTO_INCREMENT =  1").executeUpdate(); //mysql
+		entityManager.createNativeQuery("ALTER TABLE book ALTER COLUMN id RESTART WITH 1").executeUpdate(); //h2
+	}
+	
 	//BDDMockito 패턴 given, when, then
 	@Test
 	public void save_테스트() throws Exception{
@@ -61,8 +86,110 @@ public class BookControllerIntegreTest {
 			.andExpect(status().isCreated()) //201
 			.andExpect(jsonPath("$.title").value("스프링 따라하기"))
 			.andDo(MockMvcResultHandlers.print());
-			
-			
 		
 	}
+	
+	@Test
+	public void findAll_테스트() throws Exception{
+		//given
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(null,"스프링부트 따라하기","cms"));
+		books.add(new Book(null,"리엑트 따라하기","cms"));
+		books.add(new Book(null,"JUnit 따라하기","cms"));
+//		when(bookService.모두가져오기()).thenReturn(books);
+		bookRepository.saveAll(books);
+		
+		
+		//when
+		ResultActions resultAction = mockMvc.perform(get("/book")
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.[0].id").value(1L))
+			.andExpect(jsonPath("$", Matchers.hasSize(3)))
+			.andExpect(jsonPath("$.[2].title").value("JUnit 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	public void findById_테스트() throws Exception{
+		//given
+		Long id = 2L;
+		
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(null,"스프링부트 따라하기","cms"));
+		books.add(new Book(null,"리엑트 따라하기","cms"));
+		books.add(new Book(null,"JUnit 따라하기","cms"));
+		bookRepository.saveAll(books);
+		
+//		when(bookService.한건가져오기(id)).thenReturn(new Book(1L,"자바 공부하기","쌀"));
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(get("/book/{id}",id)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title").value("리엑트 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	public void update_테스트() throws Exception{
+		//given
+		Long id = 3L;
+		
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(null,"스프링부트 따라하기","cms"));
+		books.add(new Book(null,"리엑트 따라하기","cms"));
+		books.add(new Book(null,"JUnit 따라하기","cms"));
+		bookRepository.saveAll(books);
+		
+		Book book = new Book(null, "C++ 따라하기","cms");
+		String content = new ObjectMapper().writeValueAsString(book);
+//		when(bookService.수정하기(id,book)).thenReturn(new Book(1L,"C++ 따라하기","cms"));
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(put("/book/{id}",id)
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(content)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").value(3L))
+			.andExpect(jsonPath("$.title").value("C++ 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	public void delete_테스트() throws Exception{
+		//given
+		Long id = 1L;
+		
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(null,"스프링부트 따라하기","cms"));
+		bookRepository.saveAll(books);
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(delete("/book/{id}",id)
+				.accept(MediaType.TEXT_PLAIN));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+		
+		MvcResult requestResult = resultAction.andReturn();
+		String result = requestResult.getResponse().getContentAsString();
+		
+		assertEquals("ok", result);
+	}
+	
+	
+	
 }
--- src/test/java/com/cms/book/web/BookControllerUnitTest.java
+++ src/test/java/com/cms/book/web/BookControllerUnitTest.java
@@ -1,9 +1,16 @@
 package com.cms.book.web;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +18,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import com.cms.book.domain.Book;
 import com.cms.book.service.BookService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import ch.qos.logback.core.boolex.Matcher;
 import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.Matchers;
 
 
 /**
@@ -56,10 +65,90 @@ public class BookControllerUnitTest {
 			.andExpect(status().isCreated()) //201
 			.andExpect(jsonPath("$.title").value("스프링 따라하기"))
 			.andDo(MockMvcResultHandlers.print());
-			
-			
 		
 	}
+	
+	@Test
+	public void findAll_테스트() throws Exception{
+		//given
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(1L,"스프링부트 따라하기","cms"));
+		books.add(new Book(2L,"리엑트 따라하기","cms"));
+		when(bookService.모두가져오기()).thenReturn(books);
+		
+		//when
+		ResultActions resultAction = mockMvc.perform(get("/book")
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", Matchers.hasSize(2)))
+			.andExpect(jsonPath("$.[0].title").value("스프링부트 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	public void findById_테스트() throws Exception{
+		//given
+		Long id = 1L;
+		when(bookService.한건가져오기(id)).thenReturn(new Book(1L,"자바 공부하기","쌀"));
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(get("/book/{id}",id)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title").value("자바 공부하기"))
+			.andDo(MockMvcResultHandlers.print());
+	}
+	
+	@Test
+	public void update_테스트() throws Exception{
+		//given
+		Long id = 1L;
+		Book book = new Book(null, "C++ 따라하기","cms");
+		String content = new ObjectMapper().writeValueAsString(book);
+		when(bookService.수정하기(id,book)).thenReturn(new Book(1L,"C++ 따라하기","cms"));
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(put("/book/{id}",id)
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(content)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title").value("C++ 따라하기"))
+			.andDo(MockMvcResultHandlers.print());
+	}
+	
+	
+	@Test
+	public void delete_테스트() throws Exception{
+		//given
+		Long id = 1L;
+		when(bookService.삭제하기(id)).thenReturn("ok");
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(delete("/book/{id}",id)
+				.accept(MediaType.TEXT_PLAIN));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andDo(MockMvcResultHandlers.print());
+		
+		MvcResult requestResult = resultAction.andReturn();
+		String result = requestResult.getResponse().getContentAsString();
+		
+		assertEquals("ok", result);
+	}
+	
+	
 
 
 }
